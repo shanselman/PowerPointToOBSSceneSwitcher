ab1954745a53164670cb36fdf013e28fd4fbcdeb
smartcodinghub <smartcodinghub@gmail.com>
2020-09-15 21:21:09 +0200

    Interface changes

--- PresentationObsSceneSwitcher/PresentationObsSceneSwitcher/ConfigurationForm.cs
+++ PresentationObsSceneSwitcher/PresentationObsSceneSwitcher/ConfigurationForm.cs
@@ -12,10 +12,52 @@ namespace PresentationObsSceneSwitcher
 {
     public partial class ConfigurationForm : Form
     {
+        #region GUI Behavior fields
+
+        private bool manualClosing;
+        private bool showDisplay;
+
+        #endregion
+
         public ConfigurationForm()
         {
             InitializeComponent();
         }
+
+        #region GUI Behavior events
+
+        protected override void SetVisibleCore(bool value) => base.SetVisibleCore(showDisplay && value);
+
+        private void menuItemClose_Click(object sender, EventArgs e)
+        {
+            this.manualClosing = true;
+            this.notifyIcon.Dispose();
+            this.Close();
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.showDisplay = true;
+            this.Visible = !this.Visible;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void ConfigurationForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized) this.Hide();
+        }
+
+        private void ConfigurationForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!manualClosing)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+        }
+
+        #endregion
+
 
     }
 }

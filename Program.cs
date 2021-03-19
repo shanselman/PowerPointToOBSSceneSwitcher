9638979eda0ec2023e200b40736b05d07318a496
Alejandro de Arriba <adearriba@outlook.com>
2021-03-19 14:37:43 +0100

    Created a Powerpoint controller
    Encapsulate powerpoint logic inside a controller.

--- Program.cs
+++ Program.cs
@@ -3,46 +3,21 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Office.Interop.PowerPoint;
 using PowerPointToOBSSceneSwitcher.OBS;
-using PowerPointToOBSSceneSwitcher.SceneProcessors;
+using PowerPointToOBSSceneSwitcher.SceneManagers;
 //Thanks to CSharpFritz and EngstromJimmy for their gists, snippets, and thoughts.
 
 namespace PowerPointToOBSSceneSwitcher
 {
     class Program
     {
-        private static Application ppt = new Microsoft.Office.Interop.PowerPoint.Application();
-        private static IOBSManager OBS;
         static async Task Main(string[] args)
         {
-            Console.Write("Connecting to PowerPoint...");
-            ppt.SlideShowNextSlide += App_SlideShowNextSlide;
-            Console.WriteLine("connected");
+            IOBSManager OBS = new OBSManager();
 
-            Console.Write("Connecting to OBS...");
-            OBS = new OBSManager();
-            await OBS.Connect();
-            Console.WriteLine("connected");
-
-            OBS.Init();
+            PowerPointSwitcherController controller = new PowerPointSwitcherController(OBS);
+            await controller.Connect();
 
             Console.ReadLine();
         }
-
-
-        async static void App_SlideShowNextSlide(SlideShowWindow Wn)
-        {
-            if (Wn != null)
-            {
-                Console.WriteLine($"Moved to Slide Number {Wn.View.Slide.SlideNumber}");
-                //Text starts at Index 2 ¯\_(ツ)_/¯
-                var note = String.Empty;
-                try { note = Wn.View.Slide.NotesPage.Shapes[2].TextFrame.TextRange.Text; }
-                catch { /*no notes*/ }
-
-                ISceneDataProcessor reader = new PowerpointNoteProcessor(OBS);
-                reader.ProcessData(note);
-            }
-        }
-
     }
 }
